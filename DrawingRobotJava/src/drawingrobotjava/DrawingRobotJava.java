c60d7e35300ecf0b3f020005a84e1c6e6f483094
Jonas <jonas.sander171@gmail.com>
2019-10-09 12:25:20 +0200

    jonas

--- DrawingRobotJava/src/drawingrobotjava/DrawingRobotJava.java
+++ DrawingRobotJava/src/drawingrobotjava/DrawingRobotJava.java
@@ -35,6 +35,10 @@ public class DrawingRobotJava extends Application
         System.out.println("phil tester");
         System.out.println("Jonas er IKKE SEJ!");
         System.out.println("GIT ER IKKE NEMT");
+        
+        
+        
+        System.out.println("Jonas Branch");
     }
     
 }
